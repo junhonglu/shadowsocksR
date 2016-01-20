b6fbdd9b9594782b6909e01d52fede6c982d0347
piaok <lovebeyondalways@gmail.com>
2016-01-21 00:14:52 +0800

    Update shadowsocksR-service.sh

--- shadowsocksR-service.sh
+++ shadowsocksR-service.sh
@@ -1,36 +1,35 @@
-
-#!/bin/sh 
+#!/bin/bash 
 #inoder to start or stop or restart shadowsocks 
 #chkconfig:2345 80 55 
 #description:shadowsocos start|stop|restart 
-SERVER_NAME=shadowsocks 
-PROG=server.py 
-PROG_PATH=/usr/local/shadwosocks/shadowsocks/server.py
-PID_PATH=/var/run/ 
-start(){ 
+SERVER_NAME=shadowsocks
+PROG=server.py
+PROG_PATH=/usr/local/shadowsocks/shadowsocks
+PID_PATH=/var/run
+start(){
         echo "begin start" 
-        if [[ -e $PID_PATH/$SERVER_NAME.pid ]]; then 
+        if [[ -e $PID_PATH/$SERVER_NAME.pid ]]; then
                 echo "$SERVER_NAME is runing now " 
-                exit 1 
-        else 
-                python $PROG_PATH/$PROG -c /etc/shadowsocks.json -d start 
+                exit 1
+        else
+                python $PROG_PATH/$PROG -c /etc/shadowsocks.json -d start
                 echo "start sussful" 
-        fi 
-} 
-stop(){ 
+        fi
+}
+stop(){
         echo "begin stop" 
-        if [[ -e $PID_PATH/$SERVER_NAME.pid ]]; then 
-                python $PROG_PATH/$PROG -c /etc/shadowsocks.json -d stop 
+        if [[ -e $PID_PATH/$SERVER_NAME.pid ]]; then
+                python $PROG_PATH/$PROG -c /etc/shadowsocks.json -d stop
                 echo "stop sussful"      
-         
-else 
+
+else
         echo "$SERVER_NAME NOT runing"
 fi
 }
-if [ "$(id -u)" != "0" ]; then  
-    echo "This script must be run as root" 1>&2  
-    exit 1  
-fi  
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
 case $1 in
         start )
         start
@@ -45,7 +44,8 @@ case $1 in
         start
         ;;
         ** )
-        echo "Usage: $0 {start|stop|reload}" 1>&2 
+        echo "Usage: $0 {start|stop|reload}" 1>&2
         exit 1
-        ;;      
+        ;;
 esac
+                              
